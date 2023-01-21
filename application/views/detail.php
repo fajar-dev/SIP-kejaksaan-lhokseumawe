2cd111024a2856d5b64be4fba12808c1d0c6b3b9
faja rivaldi chan <fajarrivaldi2015@gmail.com>
2023-01-22 02:01:33 +0700

    penambahan export to pdf

--- application/views/detail.php
+++ application/views/detail.php
@@ -9,31 +9,53 @@
                                 <div class="mb-2 mb-lg-0">
                                     <h3 class="mb-0  text-white"><?= $title ?></h3>
                                 </div>
+                                <div>
+                                  <a href="#" onclick="cetak()" class="btn btn-white"><i class="bi bi-printer"></i> Cetak</a>
+                                </div>
                             </div>
                         </div>
                     </div>
                     <div class="offset-xl-2 col-xl-8 offset-lg-1 col-lg-10 col-md-12
               col-12">
-            <div class="row">
+            <div class="row" id="print">
               <div class="col-12 mb-6">
                 <!-- card  -->
                 <div class="card">
                   <!-- card header  -->
                   <div class="card-header p-4 bg-white">
-                    <h4 class="mb-0">Pengaju: <?php echo htmlentities($data['nama'], ENT_QUOTES, 'UTF-8');?></h4>
+                    <h4 class="mb-0">Pengaju : <?php echo htmlentities($data['nama'], ENT_QUOTES, 'UTF-8');?></h4>
                   </div>
                   <!-- card body  -->
                   <div class="card-body">
-                    <!-- row  -->
-                    <div class="mb-2">
-                      <p class="mb-2">Tipe Tamu: <?php echo htmlentities($data['tipe'], ENT_QUOTES, 'UTF-8');?></p>
-                      <p class="mb-2">Pelayanan: <?php echo htmlentities($data['pelayanan'], ENT_QUOTES, 'UTF-8');?></p>
-                      <p class="mb-2">Jaksa Tujuan: <?php echo htmlentities($jaksa['nama_jaksa'], ENT_QUOTES, 'UTF-8');?></p>
-                      <p class="mb-2">Tujuan: <br> <?php echo htmlentities($data['tujuan'], ENT_QUOTES, 'UTF-8');?></p>
-                    </div>
+                  <div class="table-responsive">
+                              <table class="table fw-normal" border="0">
+                                <tbody>
+                                    <tr>
+                                      <td width="150px">Tipe Tamu </td>
+                                      <td width="10px">:</td>
+                                      <td><?php echo htmlentities($data['tipe'], ENT_QUOTES, 'UTF-8');?></td>
+                                    </tr>
+                                    <tr>
+                                      <td>Pelayanan</td>
+                                      <td>:</td>
+                                      <td><?php echo htmlentities($data['pelayanan'], ENT_QUOTES, 'UTF-8');?></td>
+                                    </tr>
+                                    <tr>
+                                      <td>Jaksa Tujuan </td>
+                                      <td>:</td>
+                                      <td><?php echo htmlentities($jaksa['nama_jaksa'], ENT_QUOTES, 'UTF-8');?></td>
+                                    </tr>
+                                    <tr>
+                                      <td>Tujuan </td>
+                                      <td>:</td>
+                                      <td><?php echo htmlentities($data['tujuan'], ENT_QUOTES, 'UTF-8');?></td>
+                                    </tr>
+                                </tbody>
+                              </table>
+                            </div>
                   </div>
                 </div>
-                <div class="card mt-4">
+                <div class="card mt-1">
                   <!-- card body  -->
                   <div class="card-body">
                         <!-- table  -->
@@ -68,4 +90,17 @@
             </div>
         </div>
     </div>
-
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js" integrity="sha512-GsLlZN/3F2ErC5ifS5QtgpiJtWd43JWSuIgh7mbzZ8zBps+dvLusV+eNQATqgA/HdeKFVgA5v3S/cIrLF7QnIg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+                          <script>
+                            function cetak(){
+                              var element = document.getElementById('print');
+                              var opt = {
+                              margin:       0.1,
+                              filename:     'Laporan_Pengajuan_Selesai.pdf',
+                              image:        { type: 'jpeg', quality: 9 },
+                              html2canvas:  { scale: 4 },
+                              jsPDF:        { unit: 'in', format: 'A4' }
+                              };
+                              html2pdf().set(opt).from(element).save();
+                            }
+                          </script>
